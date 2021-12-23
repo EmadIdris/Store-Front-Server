08e08e433438faaed1e2017c8def1996edb5834d
Emad Idris <emadidris@outlook.com>
2021-12-23 14:23:58 +0200

    Finish Server

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+const { start } = require('./src/server');
+const { db } = require('./src/models'); 
+//----------------------------------
+db.sync().then(() => {
+    start();
+}).catch(console.error);
---
+++ src/models/books.js
@@ -0,0 +1,13 @@
+'use strict';
+const books = (sequelize, DataTypes) =>
+    sequelize.define('books', {
+        book_name: {
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        subscriberId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    });
+module.exports = books;
